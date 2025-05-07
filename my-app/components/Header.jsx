988277dd462d5f60c618456118960f151deeb01c
saltypx <136202579+saltypx@users.noreply.github.com>
2025-05-07 01:28:24 +0000

    commit message uhbbbbbbbbb

---
+++ my-app/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+function Header() {
+    return (
+      <div className="Header">
+        <h1>Hello World</h1>
+      </div>
+      );
+    }
+      export default Header
---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
